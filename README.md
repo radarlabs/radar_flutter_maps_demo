35f0bbd78f99ba02beba11f6ce2a6151c836dfda
Craig Kochis <cjkochis@gmail.com>
2023-12-29 22:18:33 -0500

    add map marker

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ This repo demonstrates how to use Radar Maps in a Flutter application.
 
 <img width="1722" alt="Screenshot 2023-12-29 at 1 38 04 PM" src="https://github.com/radarlabs/radar_flutter_maps_demo/assets/814934/1255d5ec-ec21-46ab-8d7e-2727389900bb">
 
+## Usage
+Follow instructions to [install flutter](https://docs.flutter.dev/get-started/install)
+
+Once the `flutter` command is available, from this project root run:
+```
+flutter run
+```
 
 ## TODO:
-- [ ] Use config for publishable key
-- [ ] Add Radar logo
-- [ ] Support Android
-- [ ] Place markers & map interactions
+- [] Use config for publishable key
+- [] Add Radar logo
+- [] Support Android
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,76 @@
+import 'dart:async';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 
 void main() {
   runApp(RadarMapsDemo());
 }
 
-class RadarMapsDemo extends StatelessWidget {
+class RadarMapsDemo extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Radar Maps Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MapScreen(),
-    );
-  }
+  RadarMapsState createState() => RadarMapsState();
 }
 
 String style = "radar-default-v1";
 String publishableKey = "prj_test_pk_...";
 
-class MapScreen extends StatelessWidget {
+class RadarMapsState extends State<RadarMapsDemo> {
+  late MaplibreMapController mapController;
+
+  // async helper function to load image from assets
+  Future<ui.Image> _loadImage(Uint8List img) async {
+    final Completer<ui.Image> completer = new Completer();
+    ui.decodeImageFromList(img, (ui.Image img) {
+      return completer.complete(img);
+    });
+    return completer.future;
+  }
+
+  // helper to add images to map
+  Future<void> _addImageFromAsset(String name, String assetName) async {
+    final ByteData data = await rootBundle.load(assetName);
+    final ui.Image image = await _loadImage(new Uint8List.view(data.buffer));
+    final ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    final Uint8List imgBytes = byteData!.buffer.asUint8List();
+    mapController.addImage(name, imgBytes);
+  }
+
+  // add the marker to the map as a symbol
+  void _addMarker() {
+    mapController.addSymbol(
+      SymbolOptions(
+        geometry: LatLng(37.7749, -122.4194), // Latitude and Longitude of the marker
+        iconImage: "marker",
+        iconSize: 1.5,
+      ),
+    );
+  }
+
+  // callback on map initialization
+  void _onMapCreated(MaplibreMapController controller) {
+    mapController = controller;
+  }
+
+  // callback on may style loaded (initial render)
+  void _onStyleLoaded() {
+    _addImageFromAsset("marker", "images/marker.png");
+    _addMarker();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: MaplibreMap(
-        styleString: 'https://api.radar.io/maps/styles/$style?publishableKey=$publishableKey',
-        initialCameraPosition: CameraPosition(
-          target: LatLng(40.7128, -74.0060), // example coordinates (New York City)
-          zoom: 11.0,
+    return MaterialApp(
+      home: Scaffold(
+        body: MaplibreMap(
+          onMapCreated: _onMapCreated,
+          onStyleLoadedCallback: _onStyleLoaded,
+          styleString: 'https://api.radar.io/maps/styles/$style?publishableKey=$publishableKey',
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.7749, -122.4194), // initial position of the map (San Francisco)
+            zoom: 12.0,
+          ),
         ),
       ),
     );
