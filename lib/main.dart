440a8eec930c83f19109102b64fc0cd7aa732ea8
Craig Kochis <cjkochis@gmail.com>
2024-01-02 13:26:05 -0500

    add marker placement and interactions

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,19 @@ class RadarMapsDemo extends StatefulWidget {
 }
 
 String style = "radar-default-v1";
-String publishableKey = "prj_test_pk_...";
+String publishableKey = "prj_live_pk_...";
 
 class RadarMapsState extends State<RadarMapsDemo> {
   late MaplibreMapController mapController;
+  List<Symbol> markers = [];
+  bool isCameraMovingProgrammatically = false;
+
+  // state for info window
+  LatLng? infoWindowPosition;
+  Symbol? logoMarker;
+  Symbol? selectedMarker;
+  bool showInfoWindow = false;
+  String infoWindowText = "";
 
   // async helper function to load image from assets
   Future<ui.Image> _loadImage(Uint8List img) async {
@@ -28,7 +37,7 @@ class RadarMapsState extends State<RadarMapsDemo> {
     return completer.future;
   }
 
-  // helper to add images to map
+  // helper to prepare images for map use
   Future<void> _addImageFromAsset(String name, String assetName) async {
     final ByteData data = await rootBundle.load(assetName);
     final ui.Image image = await _loadImage(new Uint8List.view(data.buffer));
@@ -38,39 +47,132 @@ class RadarMapsState extends State<RadarMapsDemo> {
   }
 
   // add the marker to the map as a symbol
-  void _addMarker() {
-    mapController.addSymbol(
+  void _addMarker(LatLng latLng) async {
+    final Symbol marker = await mapController.addSymbol(
       SymbolOptions(
-        geometry: LatLng(37.7749, -122.4194), // Latitude and Longitude of the marker
+        geometry: latLng,
         iconImage: "marker",
-        iconSize: 1.5,
+        iconSize: 1,
       ),
     );
+    markers.add(marker);
+  }
+
+  // center map on given lat/lng
+  void _centerMapOnPosition(LatLng latLng) {
+    setState(() {
+      isCameraMovingProgrammatically = true; // user is not moving the map
+    });
+
+    mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: latLng, zoom: 14.0),
+      ),
+    ).then((_) {
+      Future.delayed(Duration(seconds: 1), () {
+        setState(() {
+          isCameraMovingProgrammatically = false; // camera movement finished
+        });
+      });
+    });
+  }
+
+  // handle tap on marker
+  void _onMarkerTapped(Symbol symbol) {
+    String text = "";
+    if (symbol == logoMarker) {
+      text = "Radar HQ";
+    } else {
+      int index = markers.indexOf(symbol);
+      text = "Marker ${index + 1}";
+    }
+
+    setState(() {
+      selectedMarker = symbol;
+      infoWindowPosition = symbol.options.geometry;
+      showInfoWindow = true;
+      infoWindowText = text;
+    });
+
+    _centerMapOnPosition(symbol.options.geometry!);
+  }
+
+  // create a new InfoWindow
+  Widget _buildInfoWindow(Symbol? marker) {
+    if (marker == null) {
+      return Container();
+    }
+
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Text(infoWindowText),
+      ),
+    );
+  }
+
+  void _hideInfoWindow() {
+    if (!isCameraMovingProgrammatically) {
+      setState(() {
+        showInfoWindow = false;
+      });
+    }
   }
 
   // callback on map initialization
   void _onMapCreated(MaplibreMapController controller) {
     mapController = controller;
+    mapController.onSymbolTapped.add(_onMarkerTapped);
   }
 
   // callback on may style loaded (initial render)
-  void _onStyleLoaded() {
+  void _onStyleLoaded() async {
+    _addImageFromAsset("logo", "images/logo.png");
     _addImageFromAsset("marker", "images/marker.png");
-    _addMarker();
+
+    // add Radar logo to the map at HQ
+    final Symbol logo = await mapController.addSymbol(
+      SymbolOptions(
+        geometry: LatLng(40.7342891, -73.9910334), // Latitude and Longitude of the marker
+        iconImage: "logo",
+        iconSize: 0.5,
+      ),
+    );
+    logoMarker = logo;
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: MaplibreMap(
-          onMapCreated: _onMapCreated,
-          onStyleLoadedCallback: _onStyleLoaded,
-          styleString: 'https://api.radar.io/maps/styles/$style?publishableKey=$publishableKey',
-          initialCameraPosition: CameraPosition(
-            target: LatLng(37.7749, -122.4194), // initial position of the map (San Francisco)
-            zoom: 12.0,
-          ),
+        body: Stack(
+          children: [
+            // map
+            MaplibreMap(
+              styleString: 'https://api.radar.io/maps/styles/$style?publishableKey=$publishableKey',
+              initialCameraPosition: CameraPosition(
+                target: LatLng(40.7342891, -73.9910334), // initial position of the map (Radar HQ)
+                zoom: 12.0,
+              ),
+              onMapCreated: _onMapCreated,
+              onStyleLoadedCallback: _onStyleLoaded,
+              onCameraIdle: () => _hideInfoWindow(),
+              onMapClick: (point, latLng) async {
+                _addMarker(latLng);
+              },
+            ),
+
+            // info window
+            if (showInfoWindow && infoWindowPosition != null)
+              Positioned(
+                top: MediaQuery.of(context).padding.top + 16, // Adjust this value for desired position
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: _buildInfoWindow(selectedMarker),
+                ),
+              ),
+          ]
         ),
       ),
     );
